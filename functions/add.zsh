dc1086c43c79189861f0d4c89ff4c86b359e71e0
Kazuya Izumi <shikibu9419@gmail.com>
2019-06-28 10:38:29 +0900

    Add add extension

---
+++ functions/add.zsh
@@ -0,0 +1,11 @@
+git-extended-add() {
+  selected=$(git add -N -A; git status -s |
+             fzf -m --ansi --preview="echo {} | cut -d' ' -f3 | xargs git diff --color" |
+             cut -d' ' -f3 |
+             tr '\n' ' ')
+
+  if [ -z $selected ] && return 0
+
+  git add $=selected
+  echo "Added: $selected"
+}
