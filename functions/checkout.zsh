96a1bcb8f432bd8095aaeda4325fe794a456c97c
Kazuya Izumi <shikibu9419@gmail.com>
2019-06-28 16:10:00 +0900

    Fix bug

--- functions/checkout.zsh
+++ functions/checkout.zsh
@@ -1,6 +1,7 @@
 git-extended-checkout() {
   branch=$(git branch --all | grep -v HEAD | fzf-tmux --ansi -d)
   [ -n $branch ] && git checkout $(echo $branch | sed 's/.* //' | sed 's#remotes/[^/]*/##')
+  zle reset-prompt
 }
 
 zle -N git-extended-checkout
