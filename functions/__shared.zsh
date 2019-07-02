a9bd75387ba28ba1233f79257025f86ac13d878e
Kazuya Izumi <shikibu9419@gmail.com>
2019-07-02 19:33:19 +0900

    Add check available function

--- functions/__shared.zsh
+++ functions/__shared.zsh
@@ -9,11 +9,13 @@ __git_extended::init() {
   FZF='fzf --ansi --no-sort --reverse'
   FZF_TMUX='fzf-tmux --ansi --no-sort -d'
 
-  __git_extended::check_available
+  __git_extended::check-available
 }
 
-__git_extended::check_available() {
-  git rev-parse > /dev/null 2>&1
+__git_extended::check-available() {
+  git rev-parse > /dev/null 2>&1 &&
+    type 'fzf' > /dev/null 2>&1 &&
+    type 'hub' > /dev/null 2>&1
 }
 
 __git_extended::error() {
--- functions/browse.zsh
+++
@@ -1,9 +0,0 @@
-git-extended-browse() {
-  __git_extended::init || __git_extended::error && return 1
-  selected=$(ghq list | $=FZF_TMUX -m | tr '\n' ' ')
-
-  for dir in $=selected; do
-    repo=${dir#*/}
-    hub browse $repo
-  done
-}
