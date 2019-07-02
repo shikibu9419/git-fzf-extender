cce437fdf626583c3e7710fe599938050cbff124
Kazuya Izumi <shikibu9419@gmail.com>
2019-07-02 18:26:58 +0900

    Improve issue extension

--- functions/issue.zsh
+++ functions/issue.zsh
@@ -6,27 +6,33 @@ git-extended-issue() {
 
 __git_extended::list_issue() {
   __git_extended::init
-  stts=${1:-open}
 
-  opts="$(unbuffer hub issue -s $stts | sed 's/ *#/#/')"
-  opts="$opts\n${YELLOW}+   CREATE ISSUE\n${MAGENTA}<-> SWITCH STATUS${DEFAULT}"
+  open_issues="$(unbuffer hub issue -s open | sed 's/ *#/#/')"
+  closed_issues="$(unbuffer hub issue -s closed | sed 's/ *#/#/')"
+  opts="${YELLOW}+   CREATE ISSUE\n${MAGENTA}<-> SWITCH STATUS${DEFAULT}"
 
-  selected=$(echo -e $opts | fzf-tmux -d --ansi --no-sort --reverse --prompt="Select issue ($stts)> ")
+  stts=open
+  while true ; do
+    [ $stts = open ] && issues=$open_issues || issues=$closed_issues
 
-  case "$selected" in
-    '+'*)
-      __git_extended::create_issue
-      ;;
-    '<->'*)
-      new_status='open'
-      [ $stts = 'open' ] && new_status='closed'
-      __git_extended::list_issue $new_status
-      ;;
-    "")
-      ;;
-    *)
-      hub browse -- issues/$(echo $selected | cut -d' ' -f1 | cut -b 2-)
-  esac
+    selected=$(echo -e "$issues\n$opts" | sed 's/^[ ]*$//g' | fzf-tmux -d --ansi --no-sort --reverse --prompt="Select issue ($stts)> ")
+
+    case "$selected" in
+      '<->'*)
+        [ $stts = open ] && stts=closed || stts=open
+        continue
+        ;;
+      '+'*)
+        __git_extended::create_issue
+        ;;
+      "")
+        ;;
+      *)
+        hub browse -- issues/$(echo $selected | cut -d' ' -f1 | cut -b 2-)
+    esac
+
+    return
+  done
 }
 
 __git_extended::create_issue() {
