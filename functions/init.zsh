e47125b958b518a00f50e777063f7ce8876004ed
Kazuya Izumi <shikibu9419@gmail.com>
2019-09-28 02:08:20 +0900

    Update init script

--- functions/init.zsh
+++ functions/init.zsh
@@ -1,14 +1,34 @@
 git-extended-init() {
-  printf 'Repo name: '; read name
-  printf 'Description: '; read desc
-  printf 'Project page URL: '; read url
-  printf 'Will you make this private?: '; read -q && pri='-p'
+  stts='doing'
 
-  echo
-  git init &&
-    hub create "$name" ${desc:+-d "$desc"} ${url:+-h "$url"} ${pri:+"$pri"}
+  while [ $stts != 'done' ]; do
+    dir=${PWD##*/}
 
-  [ -f 'README.md' ] ||
-    echo "# ${PWD##*/}" >> README.md &&
-    echo 'Create README.'
+    # form
+    printf "Repo name (default: '$dir'): "; read name
+    printf 'Description: '; read desc
+    printf 'Project page URL: '; read url
+    printf 'Will you make this private?: '; read -q && pri='-p'
+    echo
+
+    [ -z $name ] && name=$dir
+
+    # confirmation
+  echo -e "
+$BOLD---
+Repository: $name
+Description: $desc
+Project page URL: $url"
+
+    [ -n $pri ] && echo 'Make private'
+
+    echo "$DEFAULT"
+
+    printf 'OK?: '; read -q && stts='done'
+    echo
+  done
+
+  git init && hub create "$name" ${desc:+-d "$desc"} ${url:+-h "$url"} ${pri:+"$pri"}
+
+  [ -f 'README.md' ] || { echo "# ${PWD##*/}" >> README.md && echo 'README is not found. Created.' }
 }
